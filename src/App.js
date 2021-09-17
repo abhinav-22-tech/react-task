c352d7845fe6c2b30994937fe6dbf8bd13cc2785
abhinav-22-tech <jainabhinav0860@gmail.com>
2021-09-17 12:27:20 +0530

    app-state conflict resolve

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import "./App.css";
+import { useState } from "react";
 import Home from "./Components/pages/Home";
+import RequirementForm from "./Components/pages/RequirementForm";
 
 function App() {
-  
+  const [appState, setappState] = useState("home");
+
+  const sendAppData = () => {
+    setappState("requirement");
+  };
+
+  const getValues = () => {
+    setappState("home");
+  };
+
   return (
     <div className="App">
-     <Home/>
+      {appState === "home" && (
+        <Home
+          data="Add Requirement"
+          sendData={sendAppData}
+          title={""}
+          description={""}
+          attribute={""}
+        />
+      )}
+      {appState === "requirement" && <RequirementForm add={getValues} />}
     </div>
   );
 }
