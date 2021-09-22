124ba535175d1e1983fde36b7cc0c917fccd00ca
Yamini Sharma <79048183+Yam27022001@users.noreply.github.com>
2021-09-22 11:36:03 +0530

    Update Home.js

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -1,14 +1,15 @@
 import "./Home.css";
 
-const Home = ({ data, title, description, attribute, sendData }) => {
+const Home = (props, data, sendData) => {
+  console.log(props);
   return (
     <>
       <div className="card-2">
         <div className="head-1">
-          <h1>{title}</h1>
+          {/* <h1>{title}</h1>
           <p>{description}</p>
           <p>{attribute}</p>
-          <hr />
+          <hr /> */}
           <button onClick={sendData}>{data}</button>
         </div>
       </div>
